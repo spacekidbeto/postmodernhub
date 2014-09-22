a436ece8cd2434411c4e2573849efcc41ec72eda
Roberto Hernandez <hernandezr@smu.edu>
2014-09-22 13:01:51 -0500

    add_film.php

---
+++ 3.2/add_film.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <title>Add a New Film</title>
+</head>
+<body>
+  <h1>Add a New Film</h1>
+
+<form action="add_film.php" method="POST">
+  <label for="title">Film Title</label>
+  <input type="text" name="title" />
+  <input type="submit" value="Add Film" />
+</form>
+
+
+<p><small><a href="src.php">Back to list</a></p>
+</body>
+</html>
