9866cf257b862fcff81f52354a6f05a12ecdf7b9
hernandezr@smu.edu <hernandezr@smu.edu>
2014-09-24 02:20:02 -0500

    got basic site running
    How do you make the tags go across the page and not under each other?

--- project04rack/albums.rb
+++ project04rack/albums.rb
@@ -2,7 +2,9 @@ require 'rack'
 
 class HelloWorld
   def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
+  	file = File.open("albums.html", "r")
+	contents=file.read 
+    [200, {"Content-Type" => "text/html"}, ["Hello from Rack!"]]
   end
 end
 
