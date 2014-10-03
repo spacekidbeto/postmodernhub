f71446b5e6e0fd87eabcecff5eead0a4784591d1
hernandezr@smu.edu <hernandezr@smu.edu>
2014-10-03 02:33:00 -0500

    proj4pt2

--- project04rack/albums.rb
+++ project04rack/albums.rb
@@ -1,18 +1,15 @@
 require 'rack'
 
 class Bums
+  
+  attr_accessor :name, :year, :rank
 
-  def init(request, response)
-    @request = request
-    @response = response
-    @albs = {}
-    File.open("top_100_albums.txt").each do |line|
-      get_props = line.split(", ")
-      album = get_props[0]
-      year = get_props[1]
-      @albs[album] = year
-    end
+  def init()
+    @name = name
+    @year = year
+    @rank = rank
   end
+
 end
 
 class Albums
@@ -35,15 +32,36 @@ class Albums
   end
 
   def render_list(request)
+    sorting_ = request.params["sort_by"]
+    ranking = request.params["highlight"].to_i
     response = Rack::Response.new
-    File.open("top.html", "rb") { |file| response.write(file.read) }
-    Bums.new(request, response)
-    File.open("bottom.html", "rb") { |file| response.write(file.read) }
+    album_obj = []
+    response.write("<!doctype html>\n <html>\n <head>\n <style type=\"text/css\"> .highlighted {background: yellow;}</style> </head>\n <body>\n");
+    response.write("<table>\n <tr>\n <th>Rank #</th>\n <th>Album Name</th>\n <th>Year Released</th>\n </tr>\n")
+    File.open("top_100_albums.txt","r").each do |collect|
+        temp = collect.split(", ")
+        b = Bums.new
+        b.name = temp[0]
+        b.year = temp[1]
+        album_obj.push(b)
+    end
+
+    sort_by(album_obj, sorting_)
+    album_obj.each_with_index do |x, i|
+      if(x.rank==ranking) then response.write( "<tr class=\"highlighted\">\n <td>\ #{x.rank}\ </td> <td> \ #{x.name}\ </td>\n <td>\ #{x.year}\ </td>\n </tr>\n")
+      else response.write(" <tr>\n <td>\ #{x.rank}\ </td> <td> \ #{x.name}\ </td>\n <td>\ #{x.year}\ </td>\n </tr>\n")
+      end
+    end
+    response.write("</table>\n </body>\n</html>\n")
     response.finish
   end
 
+  def sort_by(collection, attr)
+    collection.sort! { |a,b| a.send(attr) <=> b.send(attr)}
+  end
+
   def render_404
-    [404, {"Content-Type" => "text/plain"}, ["Nothing here!"]]
+    [404, {"Content-Type" => "text/html"}, ["Oops Nothing here!"]]
   end
 
 end
