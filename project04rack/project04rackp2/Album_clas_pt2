3f59a016d57108f1c4fd37db7c403fb004d1132a
hernandezr@smu.edu <hernandezr@smu.edu>
2014-10-08 13:22:19 -0500

    sinatra fixed

---
+++ project04rack/project04rackp2/Album_clas_pt2
@@ -0,0 +1,7 @@
+class Album
+  include DataMapper::Resource
+  property :id, Serial
+  property :rank, Integer
+  property :title, String
+  property :year, Integer
+end
--- project04rack/project04rackp2/Album_pt2.rb
+++ project04rack/project04rackp2/Album_pt2.rb
@@ -1,16 +1,8 @@
 require ‘sinatra’
 require ‘data_mapper’
-require_relative ‘album’
+require_relative ‘Album_clas_pt2’
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/albums.sqlite3.db")
 set :port, 8080
 get “/” do
   “Sinatra is working!”
-end
-
-class Album
-  include DataMapper::Resource
-  property :id, Serial
-  property :rank, Integer
-  property :title, String
-  property :year, Integer
 end
