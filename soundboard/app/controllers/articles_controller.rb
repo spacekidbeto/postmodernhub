4db3d663939dd5be597b177fa0138cdbfb2cc4e1
hernandezr@smu.edu <hernandezr@smu.edu>
2014-10-17 03:08:20 -0500

    made _form.html.erb

--- soundboard/app/controllers/articles_controller.rb
+++ soundboard/app/controllers/articles_controller.rb
@@ -1,23 +1,51 @@
 class ArticlesController < ApplicationController
   
-  def new
-  
-  end
+def new
+  @article = Article.new
+end
  
-  def create
-    @article = Article.new(article_params)
+def create
+  @article = Article.new(article_params)
  
-    @article.save
+  if @article.save
     redirect_to @article
+  else
+    render 'new'
+  end
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+ 
+def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+ def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 
   def show
     @article = Article.find(params[:id])
   end
- 
-  private
-  def article_params
-    params.require(:article).permit(:title, :text)
-  end
 
+  def index
+    @articles = Article.all
+  end
+ 
 end
